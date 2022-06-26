a3a6b29400dc53801da9a1a84697d939f418a03b
Naryond <giuggey85@gmail.com>
2022-06-26 02:47:16 +0100

    CSS Structure completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,51 @@
 import React from 'react';
+import './App.css';
+import Square from './components/Square';
 
 function App() {
-  return <div>Start</div>;
+  document.title = 'Calculator';
+
+  const renderSquare = () => {
+    return <Square />;
+  };
+
+  return (
+    <div>
+      <div className="app">Calculator</div>
+      <div className="box">
+        <div className="result"></div>
+        <div className="board-row">
+          <div className="square">{renderSquare()}</div>
+          <div className="square">{renderSquare()}</div>
+          <div className="square">{renderSquare()}</div>
+          <div className="square">{renderSquare()}</div>
+        </div>
+        <div className="board-row">
+          <div className="square">{renderSquare()}</div>
+          <div className="square">{renderSquare()}</div>
+          <div className="square">{renderSquare()}</div>
+          <div className="square">{renderSquare()}</div>
+        </div>
+        <div className="board-row">
+          <div className="square">{renderSquare()}</div>
+          <div className="square">{renderSquare()}</div>
+          <div className="square">{renderSquare()}</div>
+          <div className="square">{renderSquare()}</div>
+        </div>
+        <div className="board-row">
+          <div className="square">{renderSquare()}</div>
+          <div className="square">{renderSquare()}</div>
+          <div className="square">{renderSquare()}</div>
+          <div className="square">{renderSquare()}</div>
+        </div>
+        <div className="board-row">
+          <div className="square">{renderSquare()}</div>
+          <div className="square">{renderSquare()}</div>
+          <div className="squareL">{renderSquare()}</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Square.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../App.css';
+
+const Square = () => {
+  return <button className="button">x</button>;
+};
+
+export default Square;
